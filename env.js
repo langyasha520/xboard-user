85bfb09354feb08c5f01f066ccbd6fbe12d35753
langyasha520 <142591796+langyasha520@users.noreply.github.com>
2024-11-08 16:59:39 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+﻿// API地址
+window.routerBase = 'https://xiaofei.yizhifu.us.kg'
+window.settings = {
+  // 站点名称
+  title: '熊猫加速',
+  // 站点描述
+  description: '安全可靠的网络加速服务',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
